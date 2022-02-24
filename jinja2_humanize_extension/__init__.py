b3ce3fe01c8d9d6258a1fc55fbf1fd1f2b803fd1
Jean-Baptiste VESLIN <jbaptiste31@free.fr>
2022-02-24 09:14:30 +0100

    build: remove deprecated

--- jinja2_humanize_extension/__init__.py
+++ jinja2_humanize_extension/__init__.py
@@ -2,8 +2,6 @@ from jinja2.ext import Extension
 from humanize import naturalsize
 
 from humanize.time import (
-    abs_timedelta,
-    date_and_delta,
     naturaldate,
     naturalday,
     naturaldelta,
@@ -20,11 +18,6 @@ except ImportError:
 
 
 @eval_context
-def humanize_abs_timedelta(eval_ctx, delta):
-    return abs_timedelta(delta)
-
-
-@eval_context
 def humanize_intword(eval_ctx, value, format="%.1f"):
     return intword(value, format=format)
 
@@ -32,11 +25,6 @@ def humanize_intword(eval_ctx, value, format="%.1f"):
 @eval_context
 def humanize_naturalsize(eval_ctx, value, binary=False, gnu=False, format="%.1f"):
     return naturalsize(value, binary=binary, gnu=gnu, format=format)
-
-
-@eval_context
-def humanize_date_and_delta(eval_ctx, value, *args, now=None):
-    return date_and_delta(value, *args, now=None)
 
 
 @eval_context
@@ -53,7 +41,7 @@ def humanize_naturalday(eval_ctx, value):
 def humanize_naturaldelta(
     eval_ctx, value, months=True, minimum_unit="seconds", when=None
 ):
-    return naturaldelta(value, months=months, minimum_unit=minimum_unit, when=None)
+    return naturaldelta(value, months=months, minimum_unit=minimum_unit, when=when)
 
 
 @eval_context
@@ -61,7 +49,7 @@ def humanize_naturaltime(
     eval_ctx, value, future=False, months=True, minimum_unit="seconds", when=None
 ):
     return naturaltime(
-        value, future=future, months=months, minimum_unit=minimum_unit, when=None
+        value, future=future, months=months, minimum_unit=minimum_unit, when=when
     )
 
 
@@ -77,9 +65,7 @@ def humanize_precisedelta(
 class HumanizeExtension(Extension):
     def __init__(self, environment):
         super(HumanizeExtension, self).__init__(environment)
-        environment.filters["humanize_abs_timedelta"] = humanize_abs_timedelta
         environment.filters["humanize_naturalsize"] = humanize_naturalsize
-        environment.filters["humanize_date_and_delta"] = humanize_date_and_delta
         environment.filters["humanize_naturaldate"] = humanize_naturaldate
         environment.filters["humanize_naturalday"] = humanize_naturalday
         environment.filters["humanize_naturaldelta"] = humanize_naturaldelta
